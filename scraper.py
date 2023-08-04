e876ec6a8705f83e78fbef47c77080efb9b4c80b
Luizefz <luizefsilva2@gmail.com>
2023-08-04 20:48:11 -0300

    fix: add filter on music_name parameter

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
-from urllib.request import urlopen
 import re
+from unidecode import unidecode
+from urllib.request import urlopen
 
 def search_music(music_name):
-    print("\nSearching for " + music_name + "...\n")
-    request_url = "https://www.youtube.com/results?search_query=" + music_name.replace(" ", "+")
-    response = urlopen(request_url).read().decode() # Faz o scrape da pagina do youtube
-    find_video_id = re.findall("watch\?v=(\S{11})", response) # Pesquisa no scrape "watch\?v=(\S{11})" e devolve somente o id do video
-    return "https://www.youtube.com/watch?v=" + find_video_id[0]
+    clear_music_name = unidecode(music_name).replace(" ", "+") # Remove acentos e espaços da string
+    url_request = f"https://www.youtube.com/results?search_query={clear_music_name}+visualizer"
+    response = urlopen(url_request).read().decode() # Faz o scrape da pagina do youtube
+
+    find_video_id = re.findall("watch\?v=(\S{11})", response) # Pesquisa regex no scrape e devolve o id do video
+    return f"https://www.youtube.com/watch?v={find_video_id[0]}"
