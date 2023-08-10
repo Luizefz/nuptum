4eabb114c38de6dbd5d5cb81178aa3da8c66ae3c
Luizefz <luizefsilva2@gmail.com>
2023-08-10 20:46:22 -0300

    feat: add graph elements

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
-from scraper import search_music
-from dowloarder import download_music
+import os
+from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDRectangleFlatButton
+from scraper import download_music, search_music
 
+KV = '''
+MDScreen:
+    MDTextField:
+        id: music_name
+        hint_text: "Digite o nome da música"
+        helper_text: "Post malone one right now"
+        helper_text_mode: "on_error"
+        pos_hint: {"center_x": .5, "center_y": .6}
+        size_hint_x: .5
+
+    MDRectangleFlatButton:
+        text: "Download Music"
+        pos_hint: {"center_x": 0.4, "center_y": 0.5}
+        on_press: app.start_download(self)
+
+    MDFloatingActionButton:
+        text: "Stop music"
+        icon: "android"
+        text_color: "white"
+        pos_hint: {"center_x": .6, "center_y": .5}
+
+'''
 
 class MainApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screen = Builder.load_string(KV)
+
     def build(self):
-        self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_palette = "Green"
-
-        # download_music(search_music("astro world"))
-
-        return (
-            MDScreen(
-                
-
-                MDRectangleFlatButton(
-                    text="Hello, World",
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                    onpress= (download_music(search_music("astro world")))),
-                )
-            )
+        return self.screen
     
+    def start_download(self, instance):
+        
+        print("Finding music...")
+        music_info = search_music(self.screen.ids.music_name.text)
+        
+        print("Music found, starting download...")
+        music_info = download_music(music_info)
+
+        self.screen.ids.music_name.text = ""
+
+        print("Music downloaded successfully!")
 MainApp().run()
--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 import re
 from unidecode import unidecode
+from pytube import YouTube
 from urllib.request import urlopen
+
+musics_info = {}
 
 def search_music(music_name):
     clear_music_name = unidecode(music_name).replace(" ", "+") # Remove acentos e espaços da string
@@ -9,3 +12,41 @@ def search_music(music_name):
 
     find_video_id = re.findall("watch\?v=(\S{11})", response) # Pesquisa regex no scrape e devolve o id do video
     return f"https://www.youtube.com/watch?v={find_video_id[0]}"
+
+def download_music(url_video):
+    video = YouTube(url_video) # Choose a stream with desired audio quality
+    
+    audio_stream = video.streams.filter(only_audio=True).first() # Download the audio stream
+    
+    audio_stream.download(output_path='musics') # Create the music info dictionary
+    
+    music = {
+        'name': video.title,
+        'path_music': f'musics/{audio_stream.default_filename}'
+    } # Use slicing to get the key in musics_info
+    
+    musics_info[url_video[32:-1]] = music
+
+    return music
+
+# def download_music(url_video):
+#     video = YouTube(url_video)
+#     video_downloader = video.streams.filter(only_audio=True).first().download('musics')
+
+#     music = {
+#     'name' : video.streams[0].title,
+#     'path_music': video_downloader.replace("\\", "/")
+#     }
+
+#     musics_info[url_video[32:-1]] = music
+#     return music
+
+if __name__ == "__main__":
+    music_name = input("Enter the music name: ")
+    music_info = search_music(music_name)
+    
+    if music_info:
+        print("Music downloaded successfully!")
+        print("Music Info:", music_info)
+    else:
+        print("Music not found or could not be downloaded.")
