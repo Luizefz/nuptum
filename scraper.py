46ae4f52571f434a9d22e6d90311345991f617d3
Luizefz <luizefsilva2@gmail.com>
2023-08-21 23:04:58 -0300

    fix: downlaod permission

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ def search_music(music_name):
 
     try:
         clear_music_name = unidecode(music_name).replace(" ", "+") # Remove acentos e espaços da string
-        url_request = f"https://www.youtube.com/results?search_query={clear_music_name}+visualizer"
+        url_request = f"https://www.youtube.com/results?search_query={clear_music_name}+audio"
         response = urlopen(url_request).read().decode() # Faz o scrape da pagina do youtube
         find_video_id = re.findall("watch\?v=(\S{11})", response) # Pesquisa regex no scrape e devolve o id do video
         music_url = f"https://www.youtube.com/watch?v={find_video_id[0]}"
